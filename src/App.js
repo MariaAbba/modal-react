9f9bf258379c46f38a16da41e7b0ee993949add2
Mary <you@example.com>
2023-11-18 21:44:54 +0400

    Fixed

--- src/App.js
+++ src/App.js
@@ -3,22 +3,22 @@ import './index.scss'
 import { useState } from 'react'
 
 // ? Using the separate component
-// const Modal = ({ open, setOpen }) => (
-//   <div className={`overlay animated ${open ? 'show' : ''}`}>
-//     <div className="modal">
-//       <svg
-//         onClick={() => setOpen(!true)}
-//         height="200"
-//         viewBox="0 0 200 200"
-//         width="200"
-//       >
-//         <title />
-//         <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
-//       </svg>
-//       <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
-//     </div>
-//   </div>
-// )
+const Modal = ({ open, setOpen }) => (
+  <div className={`overlay animated ${open ? 'show' : ''}`}>
+    <div className="modal">
+      <svg
+        onClick={() => setOpen(!true)}
+        height="200"
+        viewBox="0 0 200 200"
+        width="200"
+      >
+        <title />
+        <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+      </svg>
+      <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
+    </div>
+  </div>
+)
 function App() {
   const [open, setOpen] = useState(false)
 
@@ -28,7 +28,7 @@ function App() {
         ✨ Открыть окно
       </button>
    
-       {open && (
+       {/* {open && (
         <div className="overlay">
           <div className="modal">
             <svg
@@ -44,7 +44,7 @@ function App() {
           </div>
         </div>
        )
-      }
+      } */}
       {
         // ? Conditional rendering with the separate component
         // /* {open && <Modal open={open} setOpen={setOpen}/>} */
