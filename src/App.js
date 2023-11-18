6b10c97221ab79032406477f29abd4746e219cad
Mary <you@example.com>
2023-11-18 22:39:59 +0400

    fix

--- src/App.js
+++ src/App.js
@@ -15,10 +15,14 @@ const Modal = ({ open, setOpen }) => (
         <title />
         <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
       </svg>
-      <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
+      <img
+        src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif"
+        alt="GIF"
+      />
     </div>
   </div>
 )
+
 function App() {
   const [open, setOpen] = useState(false)
 
@@ -27,8 +31,8 @@ function App() {
       <button onClick={() => setOpen(true)} className="open-modal-btn">
         ✨ Открыть окно
       </button>
-   
-       {/* {open && (
+
+      {/* {open && (
         <div className="overlay">
           <div className="modal">
             <svg
@@ -45,14 +49,10 @@ function App() {
         </div>
        )
       } */}
-      {
-        // ? Conditional rendering with the separate component
-        // /* {open && <Modal open={open} setOpen={setOpen}/>} */
-      }
 
-      {/*   
-      // ? Conditional rendering with the separate component */}
-      {/* <Modal open={open} setOpen={setOpen} /> */}
+      {/* {open && <Modal open={open} setOpen={setOpen} */}
+
+      <Modal open={open} setOpen={setOpen} />
     </div>
   )
 }
