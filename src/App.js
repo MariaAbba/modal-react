8cb8be63a0cc7aa07100799b5c776051fe6cf359
Mary <you@example.com>
2023-11-19 11:11:50 +0400

    Updated

--- src/App.js
+++ src/App.js
@@ -5,24 +5,22 @@ import {useState} from 'react'
 
 // ? Using the separate component
 
-const Modal = ({open, setOpen}) => {
-  return (
-    <div className={`overlay animated ${open ? 'show' : ''}`}>
-      <div className="modal">
-        <svg
-          onClick={() => setOpen(!open)}
-          height="200"
-          viewBox="0 0 200 200"
-          width="200"
-        >
-          <title />
-          <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
-        </svg>
-        <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
-      </div>
+const Modal = ({ open, setOpen }) => (
+  <div className={`overlay animated ${open ? 'show' : ''}`}>
+    <div className="modal">
+      <svg
+        onClick={() => setOpen(!open)}
+        height="200"
+        viewBox="0 0 200 200"
+        width="200"
+      >
+        <title />
+        <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+      </svg>
+      <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
     </div>
-  )
-}
+  </div>
+)
 
 function App() {
   const [open, setOpen] = useState(false)
